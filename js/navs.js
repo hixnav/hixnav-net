10278317f30f187a8da00f38180add6398be2388
Hixnav <100949209+hixnav@users.noreply.github.com>
2022-09-26 19:19:59 +0800

    Create navs.js

---
+++ js/navs.js
@@ -0,0 +1,46 @@
+const Navs = {
+    navs: {
+        "1-0": [{
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }, {
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }, {
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }, {
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }, {
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }, {
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }, {
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }],
+        "1-1": [{
+            logo: "https://kaifa.baidu.com/assets/favicon.ico",
+            url: "https://kaifa.baidu.com",
+            name: "开发者搜索",
+            desc: "让技术搜索更简单高效"
+        }]
+    }
+}
